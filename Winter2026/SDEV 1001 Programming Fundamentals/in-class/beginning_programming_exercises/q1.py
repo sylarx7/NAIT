a0c9af2e37064d00abb3438ec53639ebfdbab7de
Jongie Oaquiera <jongieoaquiera@gmail.com>
2026-01-15 10:14:52 -0700

    change a variable as a constant

--- Winter2026/SDEV 1001 Programming Fundamentals/in-class/beginning_programming_exercises/q1.py
+++ Winter2026/SDEV 1001 Programming Fundamentals/in-class/beginning_programming_exercises/q1.py
@@ -1,4 +1,4 @@
 #1. Ask the user for a temperature in Celsius and convert it to Fahrenheit.
 celsius = input("Temperature in *C: ")
-celsius_to_fahrenheit = (float(celsius) * (9/5) + 32)
-print(F"Celsius: {celsius} => Fahrenheit: {celsius_to_fahrenheit}")
+CELSIUS_TO_FAHRENHEIT = (float(celsius) * (9/5) + 32)
+print(F"Celsius: {celsius} => Fahrenheit: {CELSIUS_TO_FAHRENHEIT}")
